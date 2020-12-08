dffae4041092bd7bf86068730dd15fb444f42ff0
tuc48831 <tuc48831@temple.edu>
2020-12-08 11:10:17 -0500

    fix test to be properly working on tec2

--- python/tslib_so_interface_test.py
+++ python/tslib_so_interface_test.py
@@ -6,12 +6,12 @@ import tslib_so_interface as tslib
 
 class MyTestCase(unittest.TestCase):
 
-    def test_tsput(self, mock_tsput):
+    def test_tsput(self):
         tp_name = "testTuple"
         tp_value = "This is the tuple value"
         tp_size = 100
         tslib.tsput = MagicMock()
-        MagicMock.return_value = tp_size
+        MagicMock.return_value = 0
         return_value = tslib.tsput(tp_name, tp_value, tp_size)
         self.assertEqual(return_value, 0)
         tslib.tsput.assert_called_with(tp_name, tp_value, tp_size)
