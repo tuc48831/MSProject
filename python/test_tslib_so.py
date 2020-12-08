f5c0b829be32a595ae8e90d301b2588277848c17
tuc48831 <tuc48831@temple.edu>
2020-12-08 00:26:14 -0500

    move shared object to path, proper compilation/run of test tuple put/get

--- python/test_tslib_so.py
+++ python/test_tslib_so.py
@@ -2,12 +2,19 @@ import ctypes
 
 libso = ctypes.CDLL("./tslib.so")
 
-libso.tsput.argtypes = [ctypes.c_wchar_p, ctypes.c_char_p, ctypes.c_int]
+libso.tsput.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
+libso.tsget.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int]
 
 init_size = 256
-#pbuf = ctypes.create_string_buffer(init_size)
+pbuf = ctypes.create_string_buffer(init_size)
+
+return_value = libso.ts_init()
 
 tp_name = "testTuple"
 tp_value = "This is the tuple value"
 tp_size = 100
 return_value = libso.tsput(tp_name, tp_value, tp_size)
+
+return_value = libso.tsget(tp_name, pbuf, tp_size)
+
+print(pbuf.value)
