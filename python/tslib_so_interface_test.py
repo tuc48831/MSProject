1026c4ba5d73b86b8e650a40699dd963e2092236
Peter Yocum <tuc48831@temple.edu>
2020-12-08 11:07:25 -0500

    rework test due to missing patch

--- python/tslib_so_interface_test.py
+++ python/tslib_so_interface_test.py
@@ -1,20 +1,20 @@
 import unittest
-from unittest.mock import patch
+from unittest.mock import MagicMock
 
 import tslib_so_interface as tslib
 
 
 class MyTestCase(unittest.TestCase):
 
-    @patch('tslib.tsput')
     def test_tsput(self, mock_tsput):
         tp_name = "testTuple"
         tp_value = "This is the tuple value"
         tp_size = 100
-        mock_tsput.return_value = 0
+        tslib.tsput = MagicMock()
+        MagicMock.return_value = tp_size
         return_value = tslib.tsput(tp_name, tp_value, tp_size)
-        self.assertEqual(return_value, 1)
-        mock_tsput.assert_called_with(tp_name, tp_value, tp_size)
+        self.assertEqual(return_value, 0)
+        tslib.tsput.assert_called_with(tp_name, tp_value, tp_size)
 
 
 if __name__ == '__main__':
