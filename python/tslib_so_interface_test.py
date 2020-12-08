937f05dc2d1c3ef7fc9c9e76f0d4ade906ac1d2b
Peter Yocum <tuc48831@temple.edu>
2020-12-08 11:30:06 -0500

    update tsput test with succes/failure, create tsread success/failure tests

--- python/tslib_so_interface_test.py
+++ python/tslib_so_interface_test.py
@@ -1,20 +1,60 @@
 import unittest
 from unittest.mock import MagicMock
 
+import ctypes
 import tslib_so_interface as tslib
+
+buffer_return_value = "test buffer return value"
+
+
+def string_buff_side_effect(tuple_name, string_buffer, string_buffer_size):
+    new_buff = ctypes.create_string_buffer(string_buffer_size)
+    new_buff.value = buffer_return_value
+    string_buffer = new_buff
 
 
 class MyTestCase(unittest.TestCase):
 
-    def test_tsput(self):
+    def test_tsput_success(self):
         tp_name = "testTuple"
         tp_value = "This is the tuple value"
         tp_size = 100
-        tslib.tsput = MagicMock()
+        tslib.libso.tsput = MagicMock()
         MagicMock.return_value = 0
         return_value = tslib.tsput(tp_name, tp_value, tp_size)
         self.assertEqual(return_value, 0)
-        tslib.tsput.assert_called_with(tp_name, tp_value, tp_size)
+        tslib.libso.tsput.assert_called_with(tp_name, tp_value, tp_size)
+
+    def test_tsput_failure(self):
+        tp_name = "testTuple"
+        tp_value = "This is the tuple value"
+        tp_size = 100
+        tslib.libso.tsput = MagicMock()
+        # think this is the 'real' tsput err code
+        MagicMock.return_value = -104
+        return_value = tslib.tsput(tp_name, tp_value, tp_size)
+        self.assertEqual(return_value, 1)
+        tslib.libso.tsput.assert_called_with(tp_name, tp_value, tp_size)
+
+    def test_tsread_success(self):
+        tp_name = "testTuple"
+        string_buff_size = 100
+        tslib.libso.tsread = MagicMock()
+        MagicMock.return_value = 100
+        MagicMock.side_effect = string_buff_side_effect
+        return_value = tslib.tsread(tp_name, string_buff_size)
+        self.assertEqual(return_value, buffer_return_value)
+        tslib.libso.tsread.assert_called_with(tp_name, string_buff_size)
+
+    def test_tsread_failure(self):
+        tp_name = "testTuple"
+        string_buff_size = 100
+        tslib.libso.tsread = MagicMock()
+        MagicMock.return_value = 101
+        MagicMock.side_effect = string_buff_side_effect
+        return_value = tslib.tsread(tp_name, string_buff_size)
+        self.assertEqual(return_value, 1)
+        tslib.libso.tsread.assert_called_with(tp_name, string_buff_size)
 
 
 if __name__ == '__main__':
