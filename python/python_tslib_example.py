9eecf6f750ddfc2bde4edc2bbf0562eeceaea1c7
Peter Yocum <tuc48831@temple.edu>
2020-12-15 11:46:56 -0500

    update example script to test tuple name matching functionality

--- python/python_tslib_example.py
+++ python/python_tslib_example.py
@@ -15,12 +15,13 @@ return_value = tslib.tsput("date", str(datetime.datetime.now()), tuple_size)
 print("return value from tsput : " + str(return_value))
 
 # ts read v has a malloc inside the API to modify the buffer size when it returns so you don't have to allocate buffer size manually
-string_from_tuple_space = tslib.tsread("date", tuple_size)
 
+string_from_tuple_space, matched_tuple_name = tslib.tsread("da", tuple_size)
+print("matched tuple name : " + matched_tuple_name)
 print("value retrieved from tuple space : " + string_from_tuple_space)
 
 string_from_tuple_space = None
 
-string_from_tuple_space = tslib.tsget("date", tuple_size)
-
+string_from_tuple_space, matched_tuple_name = tslib.tsget("date", tuple_size)
+print("matched tuple name : " + matched_tuple_name)
 print("value retrived from tuple space with tsget : " + string_from_tuple_space)
