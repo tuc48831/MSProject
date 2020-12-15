4f650085df0e31815a1fc2e54d19d64abbf5a97c
Peter Yocum <tuc48831@temple.edu>
2020-12-14 21:47:12 -0500

    Revert "try direct import of dll for mocking in unit test"
    This reverts commit 6c0960ff5a92ee4465770447fac7d129d43c5004.

--- python/tslib_so_interface_test.py
+++ python/tslib_so_interface_test.py
@@ -15,49 +15,46 @@ def string_buff_side_effect(self, tuple_name, string_buffer, string_buffer_size)
 
 class MyTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.libso = ctypes.CDLL("./tslib.so")
-
     def test_tsput_success(self):
         tp_name = "testTuple"
         tp_value = "This is the tuple value"
         tp_size = 100
-        self.libso.tsput = MagicMock()
+        tslib.libso.tsput = MagicMock()
         MagicMock.return_value = 100
         return_value = tslib.tsput(tp_name, tp_value, tp_size)
         self.assertEqual(return_value, 0)
-        self.libso.tsput.assert_called_with(tp_name, tp_value, tp_size)
+        tslib.libso.tsput.assert_called_with(tp_name, tp_value, tp_size)
 
     def test_tsput_failure(self):
         tp_name = "testTuple"
         tp_value = "This is the tuple value"
         tp_size = 100
-        self.libso.tsput = MagicMock()
+        tslib.libso.tsput = MagicMock()
         # think this is the 'real' tsput err code
         MagicMock.return_value = -104
         return_value = tslib.tsput(tp_name, tp_value, tp_size)
         self.assertEqual(return_value, 1)
-        self.libso.tsput.assert_called_with(tp_name, tp_value, tp_size)
+        tslib.libso.tsput.assert_called_with(tp_name, tp_value, tp_size)
 
     def test_tsread_success(self):
         tp_name = "testTuple"
         string_buff_size = 100
-        self.libso.tsread = MagicMock()
+        tslib.libso.tsread = MagicMock()
         MagicMock.return_value = 100
         MagicMock.side_effect = string_buff_side_effect
         return_value = tslib.tsread(tp_name, string_buff_size)
         self.assertEqual(return_value, buffer_return_value)
-        self.libso.tsread.assert_called_with(tp_name, string_buff_size)
+        tslib.libso.tsread.assert_called_with(tp_name, string_buff_size)
 
     def test_tsread_failure(self):
         tp_name = "testTuple"
         string_buff_size = 100
-        self.libso.tsread = MagicMock()
+        tslib.libso.tsread = MagicMock()
         MagicMock.return_value = 101
         MagicMock.side_effect = string_buff_side_effect
         return_value = tslib.tsread(tp_name, string_buff_size)
         self.assertEqual(return_value, 1)
-        self.libso.tsread.assert_called_with(tp_name, string_buff_size)
+        tslib.libso.tsread.assert_called_with(tp_name, string_buff_size)
 
 
 if __name__ == '__main__':
