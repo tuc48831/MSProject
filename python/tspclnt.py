0d4fdcfb6f4cc4e3b882f97dbf676a0b2c9d505e
tuc48831 <tuc48831@temple.edu>
2020-12-20 17:38:28 -0500

    updated tspclnt with fixes

--- python/tspclnt.py
+++ python/tspclnt.py
@@ -8,7 +8,7 @@ API_KEY = None
 
 
 # arg parser that has api-key, mutually exclusive indicator to load either cost matrix/coords
-def parse_args(args):
+def parse_args():
     parser = argparse.ArgumentParser(prog='tspclnt')
     parser.add_argument('-s', '--store_tuple_space', dest='stored_tuple_name', help='store the cost matrix in the tuple space')
     parser.add_argument('-a', '--api_key', dest='api_key', help='The google maps API key for turning coords into a cost matrix, unncessary for --matrix')
@@ -17,7 +17,7 @@ def parse_args(args):
     group.add_argument('-m', '--matrix', dest='matrix_file_location', help='an input file of a raw cost matrix, mutually exclusive with --coord')
     group.add_argument('-c', '--coord', dest='coord_file_location', help='an input file of raw gps coordinates to be turned into a cost matrix, mutually exclusive with --matrix')
 
-    return parser.parse_args(args)
+    return parser.parse_args()
 
 
 def convert_coords_to_cost_matrix(address_file_location, api_key):
@@ -34,7 +34,7 @@ def convert_coords_to_cost_matrix(address_file_location, api_key):
 
 
 if __name__ == '__main__':
-    arguments = parse_args(sys.argv)
+    arguments = parse_args()
     if arguments.api_key is None and arguments.coord_file_location:
         print("API key is required to run with ")
 
@@ -47,14 +47,14 @@ if __name__ == '__main__':
         cost_matrix_file_location = arguments.matrix_file_location
 
     # either have raw file cost matrix OR file that we just generated
-    if arguments.store_tuple_space:
+    if arguments.stored_tuple_name:
         # load file into a matrix
         matrix = numpy.loadtxt(cost_matrix_file_location, delimiter=",")
         # call tsput on matrix
         print('putting matrix into tuple space: ' + str(matrix))
-        tslib.tsput("tsp_matrix", matrix, sys.getsizeof(matrix))
+        tslib.tsput("tsp_matrix", str(matrix), sys.getsizeof(matrix))
 
-        retrived_matrix = tslib.tsread("tsp_matrix", sys.getsizeof(matrix))
+        retrived_matrix, retrived_tuple_name = tslib.tsread("tsp_matrix", sys.getsizeof(matrix))
         print('retrieved matrix is: ' + str(retrived_matrix))
         print('raw retrieved matrixs is: ' + retrived_matrix)
 
