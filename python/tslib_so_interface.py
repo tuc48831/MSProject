e3ceaa5693787b93739303d0d228879ac05d58f4
Peter Yocum <tuc48831@temple.edu>
2020-12-19 21:58:54 -0500

    fix bad copy/paste with tuple_name_as_value in tsput

--- python/tslib_so_interface.py
+++ python/tslib_so_interface.py
@@ -18,9 +18,9 @@ def tsput(tuple_name, tuple_value, tuple_size):
         return 1
     tuple_name_as_buffer = ctypes.create_string_buffer(len(tuple_name))
     tuple_name_as_buffer.value = tuple_name.encode('utf-8')
-    tuple_name_as_buffer = ctypes.create_string_buffer(len(tuple_value))
-    tuple_name_as_buffer.value = tuple_value.encode('utf-8')
-    return_value = libso.tsput(tuple_name_as_buffer, tuple_value, tuple_size)
+    tuple_value_as_buffer = ctypes.create_string_buffer(len(tuple_value))
+    tuple_value_as_buffer.value = tuple_value.encode('utf-8')
+    return_value = libso.tsput(tuple_name_as_buffer, tuple_value_as_buffer, tuple_size)
     # return value checking, tsput either returns the tuple size or a negative number so this error checking is 'safe'
     # OR ts put error is defined as -106 in tslib.c, usually because tsh is not running
     if return_value != tuple_size or return_value == -106:
