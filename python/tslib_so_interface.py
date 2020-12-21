f320ff3bfdf9fb2795cdbfd1014e69dd40ef6e0b
Peter Yocum <tuc48831@temple.edu>
2020-12-21 15:20:05 -0500

    add new functions in tslib interface to differentiate raw bytes vs strings

--- python/tslib_so_interface.py
+++ python/tslib_so_interface.py
@@ -32,7 +32,7 @@ def tsput_bytes(tuple_name, tuple_value, tuple_size):
     if tuple_size <= 0:
         return 1
     tuple_name_as_buffer = ctypes.create_string_buffer(len(tuple_name))
-    tuple_name_as_buffer.value = tuple_name.encode('utf-8')
+    tuple_name_as_buffer.value = tuple_name
     return_value = libso.tsput(tuple_name_as_buffer, tuple_value, tuple_size)
     # return value checking, tsput either returns (int)ntohs(in.error) which i think is always 400 when success or a negative number so this error checking is 'safe'
     # OR ts put error is defined as -106 in tslib.c, usually because tsh is not running
@@ -53,10 +53,38 @@ def tsread(tuple_name, string_buffer_size):
     if return_value > string_buffer_size or return_value == -108:
         return 1
     else:
+        return string_buffer.value.decode('utf-8'), tuple_name_as_buffer.value.decode('utf-8')
+
+
+def tsread_bytes(tuple_name, string_buffer_size):
+    string_buffer = ctypes.create_string_buffer(string_buffer_size)
+    # unsure of possible maximum tuple length/return value
+    tuple_name_as_buffer = ctypes.create_string_buffer(2*len(tuple_name))
+    tuple_name_as_buffer.value = tuple_name.encode('utf-8')
+    return_value = libso.tsread(tuple_name_as_buffer, string_buffer, string_buffer_size)
+    # return value is the tuple size, so if it is larger than the buffer we pass in there is a problem
+    # OR ts read error is defined as -108 in tslib.c, usually because tsh is not running
+    if return_value > string_buffer_size or return_value == -108:
+        return 1
+    else:
         return string_buffer.value, tuple_name_as_buffer.value
 
 
 def tsget(tuple_name, string_buffer_size):
+    string_buffer = ctypes.create_string_buffer(string_buffer_size)
+    # unsure of possible maximum tuple length/return value
+    tuple_name_as_buffer = ctypes.create_string_buffer(2*len(tuple_name))
+    tuple_name_as_buffer.value = tuple_name.encode('utf-8')
+    return_value = libso.tsget(tuple_name_as_buffer, string_buffer, string_buffer_size)
+    # return value is the tuple size, so if it is larger than the buffer we pass in there is a problem
+    # OR ts get error is defined as -107 in tslib.c, usually because tsh is not running
+    if return_value > string_buffer_size or return_value == -107:
+        return 1
+    else:
+        return string_buffer.value.decode('utf-8'), tuple_name_as_buffer.value.decode('utf-8')
+
+
+def tsget_bytes(tuple_name, string_buffer_size):
     string_buffer = ctypes.create_string_buffer(string_buffer_size)
     # unsure of possible maximum tuple length/return value
     tuple_name_as_buffer = ctypes.create_string_buffer(2*len(tuple_name))
