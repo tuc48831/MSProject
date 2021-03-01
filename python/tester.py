e915d92f75b494ba3d561c0d82f102039dbd1f05
Peter Yocum <tuc48831@temple.edu>
2021-03-01 15:05:36 -0500

    fix stale reference to tour in tester

--- python/tester.py
+++ python/tester.py
@@ -80,8 +80,7 @@ if __name__ == '__main__':
     retrieved_effective_calcs = tsp.get_effective_calcs()
     print("retrieved effective calcs is: {}".format(retrieved_effective_calcs))
 
-    tour = tsp.Tour("1,3,2,4,6,8,5,7,9,10", 1122334455)
-    node = tsp.TspNode(10, tour)
+    node = tsp.TspNode(10, 1122334455, "1,3,2,4,6,8,5,7,9,10")
     node_name = tsp.node_prefix + "_001"
     print("node name is: {} and value is: {}".format(node_name, node.to_json()))
     tsp.put_node(node, node_name)
